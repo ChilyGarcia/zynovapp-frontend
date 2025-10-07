7dc6d1c79856a5ecc445fee1b583eb14159801a5
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2025-10-07 16:57:01 -0500

    fix:add-icon-windows

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: 'Zynovapp',
   description: 'Zynovapp',
   icons: {
-    icon: '/icons/zynovapp.svg',
+    icon: '/icons/zinovapp-windows.png',
   },
 }
 
